c2c60d87199a6c44d40f1f086f7038a3fc3c4931
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-05-15 16:14:23 +0100

    Default balance and test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,14 @@
+require 'oystercard'
 
-describe Oystercard do
-	
+	describe Oystercard do
+		it {is_expected.to respond_to(:balance)}
+
+	before :each do
+		@oystercard = Oystercard.new balance = 0
+	end
+	describe '#new' do
+		it "instance has default value of 0" do
+			@oystercard.balance.should eql 0
+		end
+	end
 end
