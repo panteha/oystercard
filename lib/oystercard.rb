7411550bb2a4f8065acfa83e5aedd54d389de0a2
Panteha <panteha.ahmadi@gmail.com>
2017-05-16 23:05:35 +0100

    Updated code

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,7 +4,6 @@ class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_CHARGE = 1
 
-
   def initialize()
     @balance = 0
     @in_journey = false
@@ -13,8 +12,8 @@ class Oystercard
   end
 
   def top_up(amount)
-      raise "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if @balance + amount > MAXIMUM_BALANCE
-      @balance = @balance + amount
+    raise "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if @balance + amount > MAXIMUM_BALANCE
+    @balance = @balance + amount
   end
 
   def deduct(amount)
@@ -23,29 +22,18 @@ class Oystercard
 
   def in_journey?
     @in_journey
-    # if @entry_station == []
-    #   @entry_station = false
-    # else
-    #   !!@entry_station
-    # end
   end
 
   def tap_in(entry_station)
     raise "Not enough funds" if @balance < 1
-        @entry_station = entry_station
-        @in_journey = true
+    @entry_station = entry_station
+    @in_journey = true
   end
-
 
   def tap_out(exit_station)
-      deduct(MINIMUM_CHARGE)
-      @in_journey = false
-      @list_of_journeys << { @entry_station => exit_station}
+    deduct(MINIMUM_CHARGE)
+    @in_journey = false
+    @list_of_journeys << { @entry_station => exit_station}
   end
-
-  # def list
-  # #new_hash = (@entry_station.zip(@exit_station)).to_h
-  # @list_of_journey = (@entry_station.zip(@exit_station)).to_h
-  # end
 
 end
